4c249ba4362707bdffe976190618743ab6abd40c
Stefanie Schubert <stefanie.schubert@hotmail.com>
2024-05-20 17:54:58 +0200

    fix layout home

--- src/components/views/home/Home.jsx
+++ src/components/views/home/Home.jsx
@@ -2,19 +2,22 @@ import React from 'react';
 import './Home.css';
 
 const Home = () => {
-    return(
-        <div className ='home'>
-            <h1>Ready to try the simulator?</h1>
-            <h3>Meet the future of learning how to drive</h3>
-            <h3>& with the goal towards your freedom!</h3>
-
-            <img src="/images/FirstPagePic.JPG" alt="First Page Pic" />
-
+    return (
+        <div className="home">
+            <header className="home-header">
+                <h1>Ready to try the simulator?</h1>
+                <h3>Meet the future of learning how to drive</h3>
+                <h3>& with the goal towards your freedom!</h3>
+            </header>
+            <section className="home-content">
+                <img src="/images/FirstPagePic.JPG" alt="First Page Pic" className="home-image" />
+                <div className="home-info">
+                    <p>Experience the most advanced driving simulator that helps you learn and master driving skills safely. Our simulator offers a realistic environment where you can practice driving under various conditions without any risk.</p>
+                    <button className="home-button">Book a Session Now</button>
+                </div>
+            </section>
         </div>
-
     );
 }
 
-
-
-export default Home; 
+export default Home;
